7778336a6a944ca2f9c5831e1ae651db2e4477ea
fxgzl11 <silverearthscorpion@yahoo.co.jp>
2015-09-11 16:25:22 +0900

    a

--- yahooWeather.js/weather.js
+++ yahooWeather.js/weather.js
@@ -1,55 +1,27 @@
 // RSSの読み取り
 // 画面は自分で作りましょう
-var http = require('http');
-var querystring = require('querystring');
-var crypto = require('crypto');
+// require.resolve('http'); // とモジュールを指定すると，ないモジュールを指定するとエラーを吐く
+var http = require('http'); // http 用のモジュールを実行
 
-// 出力するHTMLコードを用意する
-var htmlHeader = '<!DOCTYPE html>\
-<html lang="ja">\
-<head>\
-  <meta charset="utf-8">\
-  <title>天気予報</title>\
-</head>\
-<body>\
-<div class="content">\
-<h1>tenki yohou</h1>';
+var server = http.createServer(); // Web サーバーの
+// イベントハンドラーの実装
+server.on('request', function(request, response) {
+  // responseオブジェクトに，ステータスコード200でHTTPレスポンスヘッダを出力
+  response.writeHead(200);
+  // リクエストURLを出力
+  response.write('URL:  ' + request.url + '\n');
+  // HTTPメソッドを出力
+  response.write('Method:  ' + request.method + '\n');
+  // HTTPヘッダーを出力
+  Object.keys(request.headers).forEach(function (key) {
+    response.write(key + ':  ' + request.headers[key] + '\n');
+  });
+  // レスポンスの終了
+  response.end();
+});
+// ポート番号と，Vagrantファイルで指定したipアドレスを指定
+server.listen(8080, '127.0.0.1');
 
-
-var htmlFooter = '</div></body></html>';
-
-// 「<」や「>」、「&」といった文字列をエンティティに変換する
-function escapeHtmlSpecialChar(html) {
-  if (html === undefined) {
-    return '';
-  } else {
-    html = html.replace(/&/g, '&amp;');
-    html = html.replace(/</g, '&lt;');
-    html = html.replace(/>/g, '&gt;');
-    return html;
-  }
-};
-
-// http.Serverオブジェクトを作成する
-var server = http.createServer(onRequest);
-
-// requestイベントハンドラを定義する
-function onRequest(request, response) {
-  // リクエストされたパスが「/」以外の場合、404エラーを返す
-  if (request.url != '/') {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.end('Error 404: Not Found.');
-    return;
-  }
-
-  // POST以外のリクエストの場合、メインフォームを送信する
-  if (request.method != 'POST') {
-    response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-    response.write(htmlHeader);
-    response.write(htmlFooter);
-    response.end();
-    return;
-  }
 
 // Yahoo!Japan 天気予報 RSS
 var RSS = "http://rss.weather.yahoo.co.jp/rss/days/6110.xml";
@@ -73,11 +45,7 @@ function analyzeRSS(xml) {
       return 0;
     }
     // 天気の表示
-    response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-    response.write(htmlHeader);
-    response.write(parseString);
-    response.write(htmlFooter);
-    response.end();
+	response.write(body);
     // 変換の状況の確認用
     console.log(JSON.stringGIFy(obj));
 
@@ -89,11 +57,5 @@ function analyzeRSS(xml) {
   })
 }
 
-
-// 待ち受けするポートとアドレスを指定
-var PORT = 8080;
-var ADDRESS = '127.0.0.1';
-
-// 指定したポートで待ち受けを開始する
 server.listen(PORT, ADDRESS);
 console.log('Server running at http://' + ADDRESS + ':' + PORT + '/');
